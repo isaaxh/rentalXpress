0c5008f05e7d106ede2e507b5eb295995e1a5c27
Ayman <business.ayman@gmail.com>
2022-12-16 17:42:47 +0800

    adds runner

--- src/Runner.java
+++ src/Runner.java
@@ -2,19 +2,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+import Forms.Login;
 import HelperClasses.Customer;
 
 public class Runner {
    public static void main(String[] args) throws ClassNotFoundException {
-    ObjectInputStream in;
-    try {
-        in = new ObjectInputStream(new FileInputStream("Customers.txt"));
-        Customer c1 = (Customer) in.readObject();
-        System.out.println(c1.checkPassword("121"));
-    } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
+       Login login = new Login();
+       login.setVisible(true);
 
    } 
 }
